a56eb72178d48815076f92dfbdf5c954167e1407
Saima Khan <SKhan@discoveryed.com>
2025-12-24 01:09:24 +0530

    fix3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <div>
-      <h1>Welcome to My Portfolio</h1>
-    </div>
+    <main>
+      <h1>Welcome to Saima Khan Portfolio</h1>
+    </main>
   );
 }
