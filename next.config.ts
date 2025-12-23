688c1ac0949398403ff3fadcdb1ec049b6073a5b
Saima Khan <SKhan@discoveryed.com>
2025-12-24 01:03:47 +0530

    deployment fix 2

--- next.config.ts
+++ next.config.ts
@@ -1,24 +1,7 @@
 import type { NextConfig } from "next";
 
-const s3PublicUrl = process.env.NEXT_PUBLIC_S3_PUBLIC_URL || "";
-const s3Url = s3PublicUrl ? new URL(s3PublicUrl) : null;
-const s3Hostname = s3Url ? s3Url.hostname : "";
-const s3Protocol = s3Url ? s3Url.protocol.replace(":", "") : "https";
-
 const nextConfig: NextConfig = {
-  output: "export",
-  reactCompiler: true,
-  images: {
-    unoptimized: true,
-    qualities: [65, 75],
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "i.pinimg.com",
-      },
-    ],
-  },
+  output: "standalone",
 };
-
 
 export default nextConfig;
